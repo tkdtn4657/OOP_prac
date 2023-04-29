8c49c9d2cf98919cabae543e2c85f5b16bd9e886
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-29 20:24:34 +0900

    스프링 컨테이너를 사용하기 위해 @Configuration 어노테이션 추가 및 @Bean등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,32 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemoryMemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
 
     //new로 중복선언하던 부분 제거, 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
     //AppConfig을 보면 역할과 구현 클래스가 한눈에 들어온다. 전체 구성 파악 용이
-
 }
