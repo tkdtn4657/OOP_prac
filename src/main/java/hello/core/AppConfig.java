d1ddc773e2adde9be882e22c2026cac5fb13ab92
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-29 16:41:44 +0900

    AppConfig 리팩토링(중복제거 및 구성변경)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,14 +10,23 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
+    //new로 중복선언하던 부분 제거, 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
+    //AppConfig을 보면 역할과 구현 클래스가 한눈에 들어온다. 전체 구성 파악 용이
 
 }
